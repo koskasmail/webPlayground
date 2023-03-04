6171b06637ebf11b801711137e44dfc9dccb3db9
kessler yaron <koskasmail@gmail.com>
2023-03-04 18:27:17 +0000

    test

--- web-pages/999.01.test/script.js
+++ web-pages/999.01.test/script.js
@@ -1,6 +1,30 @@
+var index = 0;
 
 function test() {
-    console.log("test");
+    console.log('test' + this.index);
+    this.index++;
 }
 
-test();
+function checkValue(text, field) {
+    if ( (text !== null) && (text.length > 0) ) {
+        console.log('[' + text + '] size::' + text.length);
+        return text;
+    }
+    else {
+        console.error('else text');
+        return 'Eneter some Text into field #' + field;
+    }
+}
+
+
+function fnSubmit()
+{
+    const estext = document.getElementById("estext").value;
+    const cstext = document.getElementById("stext").value;
+    const lresult = document.getElementById("lresult");
+
+    console.log(estext + ' ' + cstext);
+    lresult.value = checkValue (estext, 1);
+}
+
+
