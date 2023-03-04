7e01af531e8eff39e6bd38d5532465a14cdf9dd4
kessler yaron <koskasmail@gmail.com>
2023-03-04 18:44:56 +0000

    test

--- web-pages/999.01.test/script.js
+++ web-pages/999.01.test/script.js
@@ -5,8 +5,8 @@ function test() {
     this.index++;
 }
 
-function checkValue(text, field) {
-    if ( (text !== null) && (text.length > 0) ) {
+function checkValueTest(text, field) {
+    if ((text !== null) && (text.length > 0)) {
         console.log('[' + text + '] size::' + text.length);
         return text;
     }
@@ -16,15 +16,25 @@ function checkValue(text, field) {
     }
 }
 
+function checkValue(field1, field2) {
+    if (
+        ((field1 !== null) && (field1.length > 0))
+        && ((field2 !== null) && (field2.length > 0))
+    ) { return true; }
+    else { return false; }
+}
 
-function fnSubmit()
-{
+
+function fnSubmit() {
     const estext = document.getElementById("estext").value;
     const cstext = document.getElementById("stext").value;
     const lresult = document.getElementById("lresult");
 
     console.log(estext + ' ' + cstext);
-    lresult.value = checkValue (estext, 1);
+    
+    // lresult.value = checkValue(estext, 1) + " " + checkValue(cstext, 2);
+    
+    lresult.value = checkValue(estext, cstext);
 }
 
 
