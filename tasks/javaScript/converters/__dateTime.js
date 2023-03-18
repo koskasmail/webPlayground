3fc6247b6424f91a0fbc79451d9f0e26ff6fca3a
kessler yaron <koskasmail@gmail.com>
2023-03-18 07:24:01 +0000

    test

--- tasks/javaScript/converters/__dateTime.js
+++ tasks/javaScript/converters/__dateTime.js
@@ -1,8 +1,18 @@
-
-// var dateTimeBasic = '2022-12-05T16:43:21.840Z'
-
 var dateTimeBasic = new Date();
-// dateTimeBasic = '2022-12-05T16:43:21.840Z';
+var dateTimeBasic = '2022-12-05T16:43:21.840Z'
 
-// console.log(dateTimeBasic.toLocaleDateString() );
-console.log(dateTimeBasic.LocaleTimeString());
+var output = [];
+var dd = dateTimeBasic.getDay();
+var mm = dateTimeBasic.getMonth();
+var yyyy = dateTimeBasic.getFullYear();
+
+   // if hh < 0 add 0 .
+   dd = dd < 10 ? "0" + dd : dd;
+   mm = mm < 10 ? "0" + mm : mm;
+   yyyy = yyyy < 10 ? "0" + yyyy : yyyy;
+
+output.push (dd);
+output.push (mm);
+output.push (yyyy);
+
+console.log(output);
